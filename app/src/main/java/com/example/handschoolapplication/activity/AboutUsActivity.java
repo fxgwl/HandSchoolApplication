0aaf9a43c7526bd2ab626b1a6b7ea99cc0463528
zanhw <zanhongwei_bsns@163.com>
2017-10-24 16:58:59 +0800

    1122

--- app/src/main/java/com/example/handschoolapplication/activity/AboutUsActivity.java
+++ app/src/main/java/com/example/handschoolapplication/activity/AboutUsActivity.java
@@ -14,7 +14,7 @@ import butterknife.OnClick;
 
 public class AboutUsActivity extends BaseActivity {
 
-    //这是昝宏伟新版的教育项目111
+    //这是昝宏伟新版的教育项目11122323232
 
     @BindView(R.id.rl_back)
     RelativeLayout rlBack;
